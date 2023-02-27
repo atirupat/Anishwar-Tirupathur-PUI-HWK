b6ae50641d4e8be3dff0e6ead7b665425e043ba6
atirupat <122955023+atirupat@users.noreply.github.com>
2023-02-27 16:15:18 -0500

    things are happening

---
+++ solution-hw5/js/shopping-cart.js
@@ -0,0 +1,13 @@
+// Note for Homework: will be using class Roll which was already defined in product-detail.js in this file
+
+// class Roll {
+//   constructor(rollType, rollGlazing, packSize, basePrice) {
+//       this.type = rollType;
+//       this.glazing =  rollGlazing;
+//       this.size = packSize;
+//       this.basePrice = basePrice;
+//   }
+// };
+
+// Creating initial four rolls in the cart
+const 
