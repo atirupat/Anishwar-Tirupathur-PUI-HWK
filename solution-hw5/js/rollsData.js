bba8c3b62022bc21d4cef3dd015c29e37e02ee36
atirupat <122955023+atirupat@users.noreply.github.com>
2023-02-27 15:32:54 -0500

    Created HW5

---
+++ solution-hw5/js/rollsData.js
@@ -0,0 +1,26 @@
+const rolls = {
+    "Original": {
+        "rollBasePrice": 2.49,
+        "imageFile": "original-cinnamon-roll.jpg"
+    },
+    "Apple": {
+        "rollBasePrice": 3.49,
+        "imageFile": "apple-cinnamon-roll.jpg"
+    },
+    "Raisin": {
+        "rollBasePrice": 2.99,
+        "imageFile": "raisin-cinnamon-roll.jpg"
+    },
+    "Walnut": {
+        "rollBasePrice": 3.49,
+        "imageFile": "walnut-cinnamon-roll.jpg"
+    },
+    "Double-chocolate": {
+        "rollBasePrice": 3.99,
+        "imageFile": "double-chocolate-cinnamon-roll.jpg"
+    },
+    "Strawberry": {
+        "rollBasePrice": 3.99,
+        "imageFile": "strawberry-cinnamon-roll.jpg"
+    }    
+};
